4c2dd04b7b7d43ed7c819ea670ffbba784874c00
Fabio Souto <fabiomsouto@gmail.com>
2017-07-22 17:01:11 +0100

    Wrap up work for feature

--- tests/UnsafeAPCFuseTest.php
+++ tests/UnsafeAPCFuseTest.php
@@ -1,7 +1,36 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
 use fabiomsouto\phuse\UnsafeAPCFuse;
 
+/**
+ * Class FuseObserver
+ * A smart-ass observer for testing purposes.
+ */
+class FuseObserver extends Assert implements SplObserver {
+
+    private $assertionValue;
+
+    public function __construct($assertionValue)
+    {
+        $this->assertionValue = $assertionValue;
+    }
+
+    /**
+     * Receive update from subject
+     * @link http://php.net/manual/en/splobserver.update.php
+     * @param SplSubject $subject <p>
+     * The <b>SplSubject</b> notifying the observer of an update.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function update(SplSubject $subject)
+    {
+        self::assertEquals($subject->blown(), $this->assertionValue);
+    }
+}
 /**
  * Class UnsafeAPCFuseTest
  * @covers UnsafeAPCFuse
@@ -40,19 +69,41 @@ class UnsafeAPCFuseTest extends TestCase {
                 $fuse->melt();
             }
 
-            // now the fuse is melted. it should stay that way for the duration of the restart period.
+            // now the fuse is blown. it should stay that way for the duration of the restart period.
             $this->assertTrue($fuse->blown());
             $this->assertFalse($fuse->ok());
 
             $start = $this->currentTimeMS();
             while (!$fuse->ok()) {
+                $this->assertTrue($fuse->blown());
                 $fuse->melt();
                 usleep(10000);
             }
             $stop = $this->currentTimeMS();
 
             $this->assertTrue($stop - $start > $R);
+            $this->assertTrue($fuse->ok());
         }
+    }
+
+    public function testMeltEvent() {
+        $fuse = new UnsafeAPCFuse("testFuse", 10, 100, 5000);
+        $observer = new FuseObserver(true);
+        $fuse->attach($observer);
+        for ($i = 0; $i < 11; $i++) {
+            $fuse->melt();
+        }
+    }
+
+    public function testResetEvent() {
+        $fuse = new UnsafeAPCFuse("testFuse", 10, 100, 1000);
+        $observer = new FuseObserver(false);
+        for ($i = 0; $i < 11; $i++) {
+            $fuse->melt();
+        }
+        sleep(1);
+        $fuse->attach($observer);
+        $this->assertTrue($fuse->ok());
     }
 
     private function currentTimeMS() {
